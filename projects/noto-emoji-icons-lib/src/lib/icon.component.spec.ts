82176e2fb7530cd5ebda740c94e8476d3d685dbe
Raphael Ochsenbein <raphael@akehir.com>
2023-07-19 00:45:13 +0200

    Fix lib test

--- projects/noto-emoji-icons-lib/src/lib/icon.component.spec.ts
+++ projects/noto-emoji-icons-lib/src/lib/icon.component.spec.ts
@@ -1,17 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotoEmojiIconComponent } from './icon.component';
 
 describe('NotoEmojiIconComponent', () => {
   let component: NotoEmojiIconComponent;
   let fixture: ComponentFixture<NotoEmojiIconComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotoEmojiIconComponent ]
-    })
-      .compileComponents();
-  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NotoEmojiIconComponent);
